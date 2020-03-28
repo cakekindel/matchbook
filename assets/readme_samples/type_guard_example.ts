024e9af460a66436f7e410ace2b45335fa6864e7
Orion Kindel <cakekindel@gmail.com>
2020-03-28 13:55:49 -0700

    rework discriminator example

---
+++ assets/readme_samples/type_guard_example.ts
@@ -0,0 +1,20 @@
+/* istanbul ignore file */
+
+import * as assert from 'assert';
+import { match, pattern, _ } from '../../src';
+
+declare let message: MessageEvent;
+
+type Fn<TFn extends (args: unknown[]) => unknown> = TFn;
+
+interface Event { /* ... */ }
+interface MessageEvent extends Event { /* ... */ }
+
+declare function isMessage(e: Event): e is MessageEvent;
+
+export const handleEvent: Fn<(e: Event) => string> = pattern(
+    match(isMessage, 'you got a message!'),
+    _('Event type not supported')
+);
+
+assert(handleEvent(message) === 'you got a message!');
